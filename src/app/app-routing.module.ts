c2c33712519e7191b97dffc9fd36bd3b24161a6d
ludo-snts <ludo.snts@gmail.com>
2023-08-31 22:17:45 +0200

    ajout du composant map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // Route par défaut
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
