ee31757dbce3c084738b4a7296ed4f4ce8db2bbd
ludo-snts <ludo.snts@gmail.com>
2023-08-31 23:43:16 +0200

    ajout de tile custom + geolocalisation + marker sur position utilisateur

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 
 @Component({
   selector: 'app-map',
@@ -7,6 +8,7 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements AfterViewInit {
+
   private map!: L.Map; // Utilisation du non-null assertion operator
 
   private initMap(): void {
@@ -15,11 +17,29 @@ export class MapComponent implements AfterViewInit {
       zoom: 15
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // tile layer de base sur leaflet
+    // const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   maxZoom: 18,
+    //   minZoom: 3,
+    // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // });
+
+    // tile layer custom (CartoDB.VoyagerLabelsUnder) voir http://leaflet-extras.github.io/leaflet-providers/preview/index.html
+    const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
       maxZoom: 18,
-      minZoom: 3,
-      // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      minZoom: 2,
     });
+
+    // ajout de la localisation de l'utilisateur au chargement de la page (si l'utilisateur accepte la géolocalisation) sinon se positionne sur les coordonnées par défaut : OK
+    this.map.locate({ setView: true, maxZoom: 16 });
+
+    // ajout du marqueur de la position de l'utilisateur sur la carte (géolocalisation)
+    this.map.on('locationfound', (e: L.LocationEvent) => {
+      const userMarker = L.marker(e.latlng); // Création d'un marqueur à la position de l'utilisateur
+      userMarker.addTo(this.map);
+    });
+
+
 
     tiles.addTo(this.map);
   }
